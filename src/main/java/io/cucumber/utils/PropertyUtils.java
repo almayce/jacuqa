65cc218c4b2de0867fe049b5bcfca37fa464a5a6
Alex Kolch <kolchsta@gmail.com>
2022-02-02 23:17:58 +0200

    ups steps

--- src/main/java/io/cucumber/utils/PropertyUtils.java
+++ src/main/java/io/cucumber/utils/PropertyUtils.java
@@ -3,7 +3,10 @@ package io.cucumber.utils;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.Assertions;
 
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class PropertyUtils {
@@ -11,15 +14,26 @@ public class PropertyUtils {
     Logger log = Logger.getLogger(this.getClass().getSimpleName());
 
     public Properties readProperties(String path) {
-        Properties properties = new Properties();
         log.info(path);
-        InputStream is = getClass().getClassLoader().getResourceAsStream(path);
-        try {
-            if (is != null)
+        Properties properties = new Properties();
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(path)) {
+            if (is != null) {
                 properties.load(is);
+            }
         } catch (Exception e) {
             Assertions.fail(e.getMessage());
         }
         return properties;
+    }
+
+    public void writeProperties(String path, HashMap<String, String> data) {
+        log.info(path);
+        Properties properties = new Properties();
+        properties.putAll(data);
+        try (OutputStream os = new FileOutputStream(path)) {
+            properties.store(os, "write comment");
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
     }
 }
